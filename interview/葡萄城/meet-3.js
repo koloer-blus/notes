404d579520145a983c274d0a72ebf4379c55f16c
baiziyu-fe <77950278+baiziyu-fe@users.noreply.github.com>
2021-08-14 22:54:27 +0800

    feat: meet-2

---
+++ interview/葡萄城/meet-3.js
@@ -0,0 +1,14 @@
+const data = [
+  { type: "ss", value: 6 },
+  { type: "sdddb", value: -3 },
+  { type: "ggsc", value: 4 },
+  { type: "dfg", value: 7 },
+];
+
+/**
+ * 
+ * @param {array} data 
+ */
+const renderMap = (data) => {
+
+}

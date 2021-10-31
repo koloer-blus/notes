672a845269aa4aa6349933a8658dab7d41f8bef5
baiziyu-fe <77950278+baiziyu-fe@users.noreply.github.com>
2021-10-31 20:25:16 +0800

    feat(JS-sort): 插入排序

--- javascript/sort/insertion.js
+++ javascript/sort/insertion.js
@@ -2,15 +2,14 @@
  * 
  * @param {number[]} array
  */
-const selectionSort = (array) => {
+const insertionSort = (array) => {
   const arr = [...array];
   const Len = array.length;
   for (let i = 0; i < Len; i++) {
-    const min = i;
-    for (let j = i + 1; j < Len; j++) {
-      if (arr[j] < arr[min]) {
-        let temp =  arr[min]
-        arr[min] = arr[j];
+    for (let j = i; j > 0 && (arr[j] < arr[j-1]); j--) {
+      if (arr[j] < arr[j-1]) {
+        let temp =  arr[j-1]
+        arr[j-1] = arr[j];
         arr[j] = temp;
       }
     }
@@ -20,4 +19,4 @@ const selectionSort = (array) => {
 
 const arr = [6,4,3,7,2,1,9,13,10];
 
-console.log(selectionSort(arr));
+console.log(insertionSort(arr));

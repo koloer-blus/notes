aab32bda19dbe4a690540e714dda96b9e32cab90
baiziyu-fe <77950278+baiziyu-fe@users.noreply.github.com>
2021-11-21 20:19:01 +0800

    feat(js-doc): 添加代码注释

--- javascript/code/js-type.js
+++ javascript/code/js-type.js
@@ -1,4 +1,9 @@
+/**
+ * JS类型判断
+ * @param {any} obj 
+ */
 function typeOf (obj) {
+  //'[object String]'
   const type =  Object.prototype.toString.call(obj).slice(8, -1).toLowerCase();
   console.log(type);
   return type;

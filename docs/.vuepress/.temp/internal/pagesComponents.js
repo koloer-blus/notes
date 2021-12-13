28de35534500396f3a4b55fb9f86dbc0e73119d0
baiziyu-fe <77950278+baiziyu-fe@users.noreply.github.com>
2021-12-13 18:14:14 +0800

    feat(vue-press): config

--- docs/.vuepress/.temp/internal/pagesComponents.js
+++ docs/.vuepress/.temp/internal/pagesComponents.js
@@ -3,14 +3,14 @@ import { defineAsyncComponent } from 'vue'
 export const pagesComponents = {
   // path: /
   "v-8daa1a0e": defineAsyncComponent(() => import(/* webpackChunkName: "v-8daa1a0e" */"C:/home/project/notes/docs/.vuepress/.temp/pages/index.html.vue")),
-  // path: /React/React%20Concurrent%20Mode.html
-  "v-61356017": defineAsyncComponent(() => import(/* webpackChunkName: "v-61356017" */"C:/home/project/notes/docs/.vuepress/.temp/pages/React/React Concurrent Mode.html.vue")),
   // path: /interview/EventLoop%E8%AF%A6%E8%A7%A3.html
   "v-b7c4f514": defineAsyncComponent(() => import(/* webpackChunkName: "v-b7c4f514" */"C:/home/project/notes/docs/.vuepress/.temp/pages/interview/EventLoop详解.html.vue")),
   // path: /interview/%E5%89%8D%E7%AB%AF%E6%A8%A1%E5%9D%97%E5%8C%96.html
   "v-2fa9fbe6": defineAsyncComponent(() => import(/* webpackChunkName: "v-2fa9fbe6" */"C:/home/project/notes/docs/.vuepress/.temp/pages/interview/前端模块化.html.vue")),
   // path: /interview/%E6%B5%8F%E8%A7%88%E5%99%A8%E8%BE%93%E5%85%A5URL%E7%BD%91%E5%9D%80%E5%8F%91%E7%94%9F%E7%9A%84%E4%BA%8B%E6%83%85.html
   "v-7c29de32": defineAsyncComponent(() => import(/* webpackChunkName: "v-7c29de32" */"C:/home/project/notes/docs/.vuepress/.temp/pages/interview/浏览器输入URL网址发生的事情.html.vue")),
+  // path: /React/React%20Concurrent%20Mode.html
+  "v-61356017": defineAsyncComponent(() => import(/* webpackChunkName: "v-61356017" */"C:/home/project/notes/docs/.vuepress/.temp/pages/React/React Concurrent Mode.html.vue")),
   // path: /%E6%96%87%E7%AB%A0%E7%AC%94%E8%AE%B0/Css%E5%AE%9E%E7%8E%B0%E7%AE%80%E5%8D%95%E5%9B%BE%E5%BD%A2.html
   "v-a61546fa": defineAsyncComponent(() => import(/* webpackChunkName: "v-a61546fa" */"C:/home/project/notes/docs/.vuepress/.temp/pages/文章笔记/Css实现简单图形.html.vue")),
   // path: /%E6%96%87%E7%AB%A0%E7%AC%94%E8%AE%B0/V8%E5%BC%95%E6%93%8ELazyParsing--%E8%BD%AC%E8%BD%BD.html
--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  base: "/notes/"
+  base: "./notes/"
 }

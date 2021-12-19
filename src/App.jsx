67233ec2b67e6469d04811f08c4ea64e07a39988
baiziyu-fe <77950278+baiziyu-fe@users.noreply.github.com>
2021-12-19 23:39:16 +0800

    feat(style): 样式更新

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import * as React from 'react';
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import PageRoutes from './router';
 import Header from 'components/header';
+
+import 'style/base.less';
 
 function App() {
 
   return (
     <div className="App">
-      <HashRouter>
+      <BrowserRouter basename="notes">
         <Header />
         <PageRoutes />
-      </HashRouter>
+      </BrowserRouter>
     </div>
   )
 }

cc9bc8f97369ba2de5a63a7160e32715e0bb7eed
blus <77950278+koloer-blus@users.noreply.github.com>
2025-09-28 02:04:35 +0000

    feat(remove): use issues

--- code/react-query-simple-demo/src/api.ts
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-export const getRepoInfo = (repoId: number) =>
-  axios.get(`https://api.github.com/repositories/${repoId}`);
-
-export const postRepoInfo = (data: any) =>
-  axios.post(`https://api.github.com/users/TanStack/followers`, data);
--- docs/FE-eslint原理窥探.md
+++
@@ -1,4 +0,0 @@
-# ESlint原理窥探
-## ESlint规则
-## 参考
-- [# 浅析eslint原理](https://mp.weixin.qq.com/s/cyNTdF9sVEcIYwssqM_DKQ)
